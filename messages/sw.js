ef856218d12042cc806d2d79ca2ecfe88e5daa6d
Vivek Singh <vivek.singh@veeam.com>
2024-02-07 22:06:39 +0100

    Open messages tab on click on notificaiton

--- messages/sw.js
+++ messages/sw.js
@@ -5,7 +5,29 @@ self.addEventListener('push', event => {
     const title = pushData.Title;
     const options = {
       body: pushData.Msg,
+      tag: "messages"
     };
 
     event.waitUntil(self.registration.showNotification(title, options));
   });
+
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({
+      type: "window"
+    })
+      .then(function (clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url == '/' && 'focus' in client)
+            return client.focus();
+        }
+        if (clients.openWindow) {
+          return clients.openWindow('https://bringsapp.com/messages');
+        }
+      })
+  );
+});
